22f92702c8671c17710e71c14bdf0c5c314a5226
axelnfs <68208747+axelnfs@users.noreply.github.com>
2022-08-09 22:15:26 -0500

    Create Function 1 - hello world.php

---
+++ php/8-kyu/Function 1 - hello world.php
@@ -0,0 +1,13 @@
+<?php
+// Make a simple function called 
+// greet that returns the most-famous 
+// "hello world!".
+
+function greet(){
+  return "hello world!";
+}
+
+$greet = greet();
+echo $greet;
+
+?>
